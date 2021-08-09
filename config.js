1060b1a31048487189ccda0aaca5ec8c3dd424c3
anzoid <jpatihullah@gmail.com>
2021-08-10 03:36:49 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,30 @@
+/*
+* Create and Export Configuration variables
+*/
+
+
+// Container for all envronments
+var environments = {};
+
+// staging (default) environments
+environments.staging = {
+  'httpPort': 3000,
+  'httpsPort': 3001,
+  'envName': 'staging'
+};
+
+// Production
+environments.production = {
+  'httpPort': 5000,
+  'httpsPort': 5001,
+  'envName': 'production'
+};
+
+// Determine whice environments was passed as a command-line argumnet
+var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// Check that  the current  env is one of the environments above, if not, default to staging
+var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+// Export the module
+module.exports = environmentToExport;
